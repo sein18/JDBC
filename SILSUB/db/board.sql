cd55a8e6f79b98c6f51cc20fd31ebeb3d400f841
tpdls7080@naver.com <tpdls7080@naver.com>
2022-05-08 17:47:17 +0900

    실습문제 해결완료

--- SILSUB/db/board.sql
+++ SILSUB/db/board.sql
@@ -1,3 +1,5 @@
+drop table board;
+
 create table BOARD(
 	no int primary key  ,
 	title varchar(100) not null  ,
@@ -6,7 +8,9 @@ create table BOARD(
 	regdate date not null  
 );
 
-drop table board;
-
-insert into board values(1,'hi','wew','ewewew',now());
+insert into board values(1,'안녕하세요','홍길동','처음 작성 하는 글입니다.',now());
+insert into board values(2,'너무 반가워요','김유신','글은 처음으로 적어보네요.',now());
+insert into board values(3,'가입했습니다.','이순신','만나서 반갑습니다 등업 부탁해요.',now());
+insert into board values(4,'무슨일이죠?','유관순','접속이 잘 안되네요ㅠ 어쪄죠?',now());
+insert into board values(5,'물어볼게 있습니다.','신사임당','탈퇴는 어떻게 하는건가요?',now());
 select *from board;
--- SILSUB/src/com/test2/Run.java
+++ SILSUB/src/com/test2/Run.java
@@ -8,19 +8,43 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Run {
 
-	public static void main(String[] args) {
+	public static Scanner sc = new Scanner(System.in);
 
-		selectAll();
+	public static void main(String[] args) {
+		while (true) {
+			System.out.println("*****Board*****");
+			System.out.println("1. selectAll [1번]");
+			System.out.println("2. insert [2번]");
+			System.out.println("3.  종 료  [3번]");
+			System.out.print("원하는 번호 입력: ");
+			int num = sc.nextInt();
+			sc.nextLine();
+			switch (num) {
+			case 1:
+				selectAll();
+				break;
+			case 2:
+				insert();
+				break;
+			case 3:
+				System.out.println("시스템이 종료 되었습니다.");
+				return;
+			}
+		}
 	}
 
 	public static void selectAll() {
+		System.out.println("**************전체 정보*****************");
+
 		Connection con = getConnection();
 		Statement stmt = null;
 		ResultSet res = null;
 		List<Board> list = new ArrayList<Board>();
+
 		try {
 			stmt = con.createStatement();
 			res = stmt.executeQuery("SELECT * FROM BOARD");
@@ -45,18 +69,54 @@ public class Run {
 			System.out.println(list.get(i));
 
 		}
+		System.out.println("**************************************");
 	}
 
 	public static void insert() {
+		System.out.println("**************정보 추가*****************");
 		Connection con = getConnection();
 		PreparedStatement pstm = null;
-		
+		int res = 0;
+
+		Board b = new Board();
+
+		System.out.print("no입력: ");
+		int no = sc.nextInt();
+		sc.nextLine();
+		b.setB_no(no);
+		System.out.print("title입력: ");
+		String title = sc.nextLine();
+		b.setB_title(title);
+		System.out.print("writer입력: ");
+		String writer = sc.nextLine();
+		b.setB_writer(writer);
+		System.out.print("content입력: ");
+		String content = sc.nextLine();
+		b.setB_content(content);
+
+		try {
+			pstm = con.prepareStatement("INSERT INTO BOARD VALUES(?,?,?,?,NOW())");
+			pstm.setInt(1, b.getB_no());
+			pstm.setString(2, b.getB_title());
+			pstm.setString(3, b.getB_writer());
+			pstm.setString(4, b.getB_content());
+			res = pstm.executeUpdate();
+
+			if (res != 0) {
+				System.out.println("정보가 추가되었습니다.");
+				Commit(con);
+			} else {
+				System.out.println("정보를 추가하지 못했습니다.");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			Close(pstm);
+			Close(con);
+		}
+		System.out.println("**************************************");
 	}
-
-	public static void select() {
-
-	}
-
+	//커넥션연결
 	public static Connection getConnection() {
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
@@ -76,6 +136,7 @@ public class Run {
 
 	}
 
+	// close 예외 처리
 	public static void Close(Connection con) {
 		try {
 			con.close();
@@ -98,5 +159,24 @@ public class Run {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+	}
+
+	// 커밋,롤백 예외처리
+	public static void Commit(Connection con) {
+		try {
+			con.commit();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void Rollback(Connection con) {
+
+		try {
+			con.rollback();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
 	}
 }
